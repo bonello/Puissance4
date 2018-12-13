78917b210ed87894181804de00d8512aa3ae60fc
bonello <43816353+bonello@users.noreply.github.com>
2018-12-13 17:24:41 +0100

    Add files via upload

---
+++ config.h
@@ -0,0 +1,35 @@
+/* PARAMETRES ORIGINE */
+
+#define SCREEN_WIDTH  1200
+#define SCREEN_HEIGHT 950
+#define BACKGROUND "Images/fond.bmp"
+#define PI 3.14159
+
+/* PARAMETRES DES OBJETS */
+
+#define PERSONNAGE1 "Images/snake1.bmp"
+#define PERSONNAGE2 "Images/snake2.bmp" 
+#define PERSONNAGE3 "Images/snake3.bmp"
+#define PERSONNAGE4 "Images/snake.bmp" 
+#define PERSONNAGE5 "Images/snake4.bmp"
+
+#define TAILLE_HEROS_X 32.25//38
+#define TAILLE_HEROS_Y 27 
+
+#define BULLE "Images/bulle.bmp" //
+
+#define PETIT  "Images/petit.bmp"
+
+#define SERPENT2  "Images/kaa.bmp"  
+
+#define SERPENT "Images/serpent.bmp"
+
+#define SERPENT1 "Images/serpent1.bmp"
+
+#define BOSS "Images/boss.bmp"
+
+#define GAMEOVER  "Images/gameover.bmp"
+
+#define WIN  "Images/win.bmp"
+
+
